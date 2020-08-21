9e51fc33cf50f079b7f9aef3217143eb21fb1352
dang <dangcloneben1@gmail.com>
2020-08-21 22:48:01 +0700

    Week 2

---
+++ Week2/bai4/src/main/java/bai4.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class bai4 {
+    public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 4; i++){
+            executor.execute(new Call());
+        }
+
+        System.out.println(System.currentTimeMillis() - startTime);
+
+        executor.shutdown();
+    }
+}
